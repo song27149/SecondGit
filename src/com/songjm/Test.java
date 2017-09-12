0b0b53f340ca567690613b92001975cf4e1c6add
SONG <song27149@users.noreply.github.com>
2017-09-12 23:17:06 +0800

    s

---
+++ src/com/songjm/Test.java
@@ -0,0 +1,15 @@
+package com.songjm;
+/**
+ * Copyright (c) 2017, XX科技股份有限公司 All rights reserved.
+ * 文件名称： Test.java
+ * 作 者： SONG
+ * 创建日期：2017年9月12日
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
